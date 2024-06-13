6e4d8e39985efe03b5b56fdd425ac975ed1e2764
diva23 <121968619+diva23@users.noreply.github.com>
2024-06-13 11:09:54 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+      title: 'G-Protech',
       theme: ThemeData(
         // This is the theme of your application.
         //
