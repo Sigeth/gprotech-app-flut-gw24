363647e86f31e3b2d874b8184fe70b8438bfb5b0
diva23 <121968619+diva23@users.noreply.github.com>
2024-06-16 16:26:22 +0200

    "Updated main.dart to import Typedechets and set it as the home page."

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:gprotech_app_flut_gw24/analyse_phase/runanalyse.dart';
 import 'package:gprotech_app_flut_gw24/analyse_phase/finalresult.dart';
-//import 'package:flutter/typedechets.dart';
-//import "bluetooth.dart";
 
 void main() {
   runApp(MyApp());
- 
 }
-class MyApp extends StatelessWidget {
-  
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FinalResult()
+      home: HomePage(),
     );
   }
 }
-
-
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -28,38 +21,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor : const Color.fromARGB(255, 158, 244, 201),
+      backgroundColor: const Color(0xFF81C784), // Light green background color
       body: Center(
         child: Container(
-          height :300,
-          width :300,
-          color :const Color.fromARGB(255, 200, 47, 36),
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 198, 21, 18), // Red color for container
+            borderRadius: BorderRadius.circular(20), // Rounded corners
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // Shadow position
+              ),
+            ],
+          ),
           child: const Column(
             mainAxisAlignment: MainAxisAlignment.center,
-             children: <Widget>[
-            Text(
-              'G-Protech',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            children: <Widget>[
+              Text(
+                'G-Protech',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black45,
+                      offset: Offset(2.0, 2.0),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Veolia',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+              SizedBox(height: 20),
+              Text(
+                'Veolia',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black45,
+                      offset: Offset(2.0, 2.0),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
           ),
         ),
-      
-            ),
-          
-        );
-
+      ),
+    );
   }
 }
